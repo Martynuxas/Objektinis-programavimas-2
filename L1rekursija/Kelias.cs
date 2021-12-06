8fdcd1a9a282dcf5dc0aa289d3cc49a43dfdbb47
Martynas Kemežys <60730890+Martynuxas@users.noreply.github.com>
2021-12-06 15:58:54 +0200

    Add files via upload

---
+++ L1rekursija/Kelias.cs
@@ -0,0 +1,25 @@
+﻿namespace L1rekursija
+{
+    /// <summary>
+    /// Konteinerinė klasė, skirta kelionių tarp atskirų punktų kainoms
+    /// </summary>
+    public class Kelias
+    {
+        private int[] Kainos;
+        public int Kiekis { get; private set; }
+
+        public Kelias(int dydis)
+        {
+            Kainos = new int[dydis];
+            Kiekis = 0;
+        }
+        public void PridetiKaina(int kaina)
+        {
+            Kainos[Kiekis++] = kaina;
+        }
+        public int GautiKaina(int i)
+        {
+            return Kainos[i];
+        }
+    }
+}
